8abd34a48abbdf1679b0ca6826d9c9f65fb2529d
hasanAbdullah9825 <hasan.techtomorrow@gmail.com>
2025-02-18 22:22:38 +0100

    oop modificatin

--- Controller/taskController.php
+++ Controller/taskController.php
@@ -2,100 +2,115 @@
 
 include "../config/database.php";
 include "../Model/Task.php";
-
 session_start();
-$taskModel = new Task($connection);
 
-if (! isset($_POST['csrf_token']) || ! isset($_SESSION['csrf_token']) || $_SESSION['csrf_token'] != $_POST['csrf_token']) {
-    throw new Exception("Invalid CSRF token");
-}
-function cleanInput($data)
+class TaskController
 {
 
-    return htmlspecialchars(strip_tags(trim($data)));
+    private $taskModel;
+    public function __construct($dbConnection)
+    {
+        $this->taskModel = new Task($dbConnection);
+    }
+
+    private function cleanInput($data)
+    {
+
+        return htmlspecialchars(strip_tags(trim($data)));
+    }
+    private function checkCsrfToken()
+    {
+
+        if (! isset($_POST['csrf_token']) || ! isset($_SESSION['csrf_token']) || $_SESSION['csrf_token'] != $_POST['csrf_token']) {
+            throw new Exception("Invalid CSRF token");
+        }
+    }
+
+    public function handleRequest()
+    {
+
+        try {
+            $this->checkCsrfToken();
+
+            if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+                if (isset($_POST["add_task"])) {
+                    $this->addTask();
+                } elseif (isset($_POST['delete_task'])) {
+                    $this->deleteTask();
+                } elseif (isset($_POST['complete_task'])) {
+                    $this->completeTask();
+                } elseif (isset($_POST['edit-form'])) {
+                    $this->editTask();
+                }
+
+            }
+        } catch (Exception $e) {
+            echo "Error: " . $e->getMessage();
+            exit;
+        }
+    }
+
+    private function addTask()
+    {
+        $title = $this->cleanInput($_POST['title']);
+
+        $description = $this->cleanInput($_POST['description']);
+        $due_date    = $this->cleanInput($_POST['due_date']);
+        if (empty($title) || empty($description) || empty($due_date)) {
+
+            throw new Exception("All fields are required");
+        }
+        if ($this->taskModel->addTask($title, $description, $due_date)) {
+            header("Location:../index.php");
+            exit();
+        } else {
+            throw new Exception("Failed to add task");
+        }
+
+    }
+
+    private function deleteTask()
+    {
+        $id = $this->cleanInput($_POST['delete_task']);
+
+        if ($this->taskModel->deleteTask($id)) {
+            header("Location:../index.php");
+            exit();
+        } else {
+            throw new Exception("Failed to delete task");
+        }
+
+    }
+
+    private function completeTask()
+    {
+        $id = $this->cleanInput($_POST['complete_task']);
+        if ($this->taskModel->completeTask($id)) {
+            header("Location:../index.php");
+            exit();
+        } else {
+            throw new Exception("Failed to complete task");
+        }
+
+    }
+
+    private function editTask()
+    {
+        $id          = $this->cleanInput($_POST['id']);
+        $title       = $this->cleanInput($_POST['title']);
+        $description = $this->cleanInput($_POST['description']);
+        $due_date    = $this->cleanInput($_POST['due_date']);
+
+        if ($this->taskModel->updateTask($id, $title, $description, $due_date)) {
+            header("Location:../index.php");
+            exit();
+        } else {
+            throw new Exception("Failed to update task");
+        }
+
+    }
 }
 
-try{
+$controller = new TaskController($connection);
 
-//add new task
-if($_SERVER['REQUEST_METHOD']=='POST' && isset($_POST['add_task'])){
-    //validation
-    $title = cleanInput($_POST['title']);
-
-    $description = cleanInput($_POST['description']);
-    $due_date = cleanInput($_POST['due_date']);
-    if(empty($title) || empty($description) || empty($due_date)){
-
-        throw new Exception("All fields are required");
-    }
-if($taskModel->addTask($title, $description, $due_date)){
-    header("Location:../index.php");
-    exit();
-}
-else{
-    throw new Exception("Failed to add task");
-}
-
-}
-
-
-//delete task
-if($_SERVER['REQUEST_METHOD']=='POST' && isset($_POST['delete_task'])){
-
-    $id = cleanInput($_POST['delete_task']);
-
-    if($taskModel->deleteTask($id)){
-        header("Location:../index.php");
-        exit();
-    }
-    else{
-        throw new Exception("Failed to delete task");
-    }
-}
-
-//complete task
-
-if($_SERVER['REQUEST_METHOD']=='POST' && isset($_POST['complete_task'])){
-
-    $id = cleanInput($_POST['complete_task']);
-
-    
-
-    if($taskModel->completeTask($id)){
-        header("Location:../index.php");
-        exit();
-    }
-    else{
-        throw new Exception("Failed to complete task");
-    }
-}
-
-// edit task
-
-if($_SERVER['REQUEST_METHOD']=='POST' && isset($_POST['edit-form'])){
-
-    $id = cleanInput($_POST['id']);
-    $title = cleanInput($_POST['title']);
-  
-    $description = cleanInput($_POST['description']);
-    $due_date = cleanInput($_POST['due_date']);
-
-    if ($taskModel->updateTask($id, $title, $description, $due_date)) {
-        header("Location:../index.php");
-        exit();
-    } else {
-        throw new Exception("Failed to update task");
-    }
- }
-
-
-
-}
-
-
-
-
-    catch(Exception $e){
-        echo $e->getMessage();
-    
-    }
+$controller->handleRequest();
--- Model/Task.php
+++ Model/Task.php
@@ -9,43 +9,59 @@ class Task{
     
     
 public function addTask($title,$description,$due_date){
-
-    if ($this->connection === null) {
-        return false; 
-    }
+try{
 
 $query = "INSERT INTO tasks (title,description,due_date) VALUES(?,?,?)";
 $stmt=$this->connection->prepare($query);
   
-  if ($stmt === false) {
-    return false;
+  if (!$stmt ) {
+    throw new Exception("Error preparing SQL statement: ". $this->connection->error);
 }
 $stmt->bind_param("sss", $title, $description, $due_date);
 
 return $stmt->execute();
 }
 
+catch(Exception $e){
+    echo "An error occured:".$e->getMessage();
+    return false;
+}
+    
+}
+
 
 public function getAllTasks(){
 
+  try
+{
     $query = "select *from tasks order by due_date asc";
     $stmt = $this->connection->prepare($query);
     $stmt->execute();
    return $stmt->get_result();
-    
-
+} 
+catch(Exception $e){   
+echo "an error occured while get all tasks , error is :".$e->getMessage();
 }
-
+}
 
 public function deleteTask($id){
 
+try{
     $query = "DELETE FROM tasks where id =?";
     $stmt = $this ->connection->prepare($query);
-    if ($stmt === false) {
-        return false;
+   
+    if(!$stmt){
+        throw new Exception("Query preparation faild");
     }
     $stmt->bind_param("i",$id);
+  
     return $stmt->execute();
+}
+catch(Exception $e){
+
+echo "An error occured while deleting task, error is :".$e->getMessage();
+return false;
+}
 
 }
 
@@ -62,12 +78,20 @@ public function completeTask($id){
 }
 
 public function updateTask($id, $title, $description, $due_date){
+   try{
+
     $query ="UPDATE tasks SET title=?, description=?, due_date=? where id =?";
     $stmt = $this->connection->prepare($query);
     if ($stmt === false) {
-        return false;
+        throw new Exception("Error preparing SQL statement: ". $this->connection->error);
     }
     $stmt->bind_param("sssi", $title, $description, $due_date, $id);
     return $stmt->execute();
+   }
+
+   catch(Exception $e){
+    echo "An error occured while updating task, error is :".$e->getMessage();
+    return false;
+   }
 }
 }
